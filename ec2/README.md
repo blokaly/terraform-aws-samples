16bcc5d6ee0e4e84bd84b65caa798555b07ac82d
blokaly <blokaly@users.noreply.github.com>
2020-12-07 18:46:50 +0800

    added ssh connection to ec2 instance

--- ec2/README.md
+++ ec2/README.md
@@ -1,10 +1,37 @@
 How to run
 ---
 
+- generate ssh key
+
+Under current folder, execute the following commands, use `key` as the file name:
+
+```shell
+$ ssh-keygen -t rsa
+Generating public/private rsa key pair.
+Enter file in which to save the key (/Users/charlie/.ssh/id_rsa): key
+Enter passphrase (empty for no passphrase):
+Enter same passphrase again:
+Your identification has been saved in key.
+Your public key has been saved in key.pub.
+The key fingerprint is:
+......
+
+$ chmod 400 key*
+```
+
+- run terraform
+
 ```shell
 $ terraform init
 $ terraform plan
 $ terraform apply
+```
+SSH connect
+---
+
+```shell
+$ terraform output ip
+$ ssh -i "key" ec2-user@<ip address>
 ```
 
 Clean up
@@ -20,3 +47,4 @@ Reference
 - https://registry.terraform.io/modules/terraform-aws-modules/ec2-instance/aws/latest
 - https://github.com/terraform-aws-modules/terraform-aws-ec2-instance/blob/master/examples/basic/main.tf  
 - https://letslearndevops.com/2018/08/23/terraform-get-latest-centos-ami/
+- https://dev.to/aakatev/deploy-ec2-instance-in-minutes-with-terraform-ip2
--- ec2/main.tf
+++ ec2/main.tf
@@ -4,6 +4,11 @@ provider "aws" {
   profile = "default"
 }
 
+resource "aws_key_pair" "my-key" {
+  key_name   = "my-key"
+  public_key = file("key.pub")
+}
+
 data "aws_ami" "amazon_linux" {
   most_recent = true
   owners = ["amazon"]
@@ -38,40 +43,70 @@ module "vpc" {
   }
 }
 
-output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = module.vpc.vpc_id
+resource "aws_security_group" "my-security-group" {
+  name        = "my-security-group"
+  description = "Allow HTTP, HTTPS and SSH traffic"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "terraform"
+  }
 }
 
-module "security_group" {
-  source = "terraform-aws-modules/security-group/aws"
-  version = "~> 3.0"
-
-  name = "my-security-group"
-  description = "Security group for example usage with EC2 instance"
-  vpc_id = module.vpc.vpc_id
-
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules = ["ssh-22-tcp", "all-icmp"]
-  egress_rules = ["all-all"]
-}
-
-module "ec2_with_t3_nano" {
-  source = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 2.0"
-
-  instance_count = 1
-  name = "my-t3-nano"
+resource "aws_instance" "my-t3-nano" {
 
   ami = data.aws_ami.amazon_linux.id
   instance_type = "t3.nano"
+  key_name = aws_key_pair.my-key.key_name
   monitoring = true
-  subnet_id = element(module.vpc.private_subnets, 0)
-  vpc_security_group_ids = [module.security_group.this_security_group_id]
+  subnet_id = element(module.vpc.public_subnets, 0)
+  vpc_security_group_ids = [aws_security_group.my-security-group.id]
   associate_public_ip_address = true
+
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    private_key = file("key")
+    host     = self.public_ip
+  }
 
   tags = {
     Terraform   = "true"
     Environment = "dev"
   }
+}
+
+output "ip" {
+  value = aws_instance.my-t3-nano.*.public_ip
 }
